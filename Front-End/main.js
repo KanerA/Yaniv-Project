00c2670299e91984331b17e5f17823d61c973271
Assaf Kaner <kaner.assaf@gmail.com>
2021-02-28 18:56:14 +0200

    display players' cards on screen
    using SVG images, showing the hand of each player.

--- Front-End/main.js
+++ Front-End/main.js
@@ -2,9 +2,10 @@
 const player1Input = document.getElementById("player1Name");
 const player2Input = document.getElementById("player2Name");
 const startButton = document.querySelector('.startBtn');
-const playerList = [];
+const table = document.getElementById('table');
 
 function startGame(){
+    const playerList = [];
     new URLSearchParams(window.location.search).forEach((value, name) => {
         playerList.push(new Player(value));
     })
@@ -21,6 +22,46 @@ function startGame(){
     newRound.pileDeck = pileDeck;
     newRound.tableDeck = deck;
     newRound.players = playerList;
+    playerList.forEach((_, index) => {
+        const playerHand = document.createElement('div');
+        for(let card of playerList[index].hand.cards){
+            const playerCard = document.createElement('div');
+            playerCard.classList.add('playerCard')
+            let cardImg = document.createElement('img');
+            switch(card.suit){
+                case "spades":
+                    cardImg.setAttribute('src', `../cards/${card.rank}S.svg`);
+                    playerCard.append(cardImg);
+                    break;
+                    
+                case "hearts":
+                    cardImg.setAttribute('src', `../cards/${card.rank}H.svg`);
+                    playerCard.append(cardImg);
+                    break;
+                    
+                case "diamonds":            
+                    cardImg.setAttribute('src', `../cards/${card.rank}D.svg`);
+                    playerCard.append(cardImg);
+                    break;
+                        
+                case "clubs":
+                    cardImg.setAttribute('src', `../cards/${card.rank}C.svg`);
+                    playerCard.append(cardImg);
+                    break;
+
+                case undefined:
+                    cardImg.setAttribute('src', `../cards/Black_joker.svg`);
+                    playerCard.append(cardImg);
+                    break;
+
+            }
+            playerHand.append(playerCard)
+        }
+        const line = document.createElement('hr');
+        table.append(playerHand);
+        table.append(line);
+
+    })
 }
 
 
